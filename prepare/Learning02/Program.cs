3c9f7dfa8062a8a3fdbabf1ad4c77657463b8fda
achkts <thi01003@byui.edu>
2023-04-25 21:49:06 -0600

    Learning 02 modified and completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        // Create a new job instance named job1
+        Job job1 = new Job();
+        
+
+        // Set the member variables using the dot notation & Company name
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create a second job, set its variables, display this company on the screen as well.
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Console.WriteLine(job1.Display());
+        // Console.WriteLine(job2.Display());
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Console.WriteLine(myResume._jobs[0]._jobTitle);
+        
+        myResume.Display();
+        
+
+
+
+
+
+
     }
 }
