bb689d76cc827222babec94a82208fdd0cb88a3c
achkts <thi01003@byui.edu>
2023-05-12 11:30:05 -0600

    menu selector and attributes

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Activity
+{
+    // Attributes:
+    private string _activityName;
+    private string _description;
+    private string _durationInSeconds;
+
+    
+    // Display starting message.
+
+    // Display ending message.
+
+    // Pause while showing a spinner.
+
+    // Pausing while showing a countdown timer.
+
+
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        Activty activity = new Activity();
+
+        int menuUserInput = 0;
+
+        // Create menu and user input variables
+        List<string> menu = new List<string>
+        {
+            "\nPlease select one of the following choices:",
+            "1. Start breathing activity",
+            "2. Start reflecting activity",
+            "3. Start listing activity",
+            "4. Quit",
+            "\nWhat would you like to do? "
+        };
+
+        // While user input is not 4
+        do
+        {
+            foreach(string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+            try{
+            menuUserInput = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                menuUserInput = 0;
+                Console.WriteLine("I'm sorry, I didn't understand that. Please try again.");
+            }
+
+            // Switch statement for user input.
+            if (menuUserInput == 1)
+            {
+                // Start breathing activity.
+                journal.CreateJournalEntry();
+            }
+            else if (menuUserInput == 2)
+            {
+                // Start reflecting activity.
+                journal.DisplayJournalEntries();
+            }
+            else if (menuUserInput == 3)
+            {
+                // Start listing activity.
+                journal.SaveToCSV();
+            }
+            else 
+            {
+                Console.WriteLine("Thank you for taking care of yourself today!");
+            }
+        } while (menuUserInput != 5);
     }
 }
